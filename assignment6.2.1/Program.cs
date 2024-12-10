90a6850f2ba33919b16b5e27eb5b3ae072bf9ff7
connor gammill <connor.gammill@outlook.com>
2024-12-10 10:47:58 -0500

    Add project files.

---
+++ assignment6.2.1/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace assignment6._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+		    Stack<int> numbers = new Stack<int>();
+
+            numbers.Push(1);
+            numbers.Push(2);
+            numbers.Push(3);
+            numbers.Push(4);
+            numbers.Push(5);
+
+            foreach( int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("Popping elements off the stack:");
+
+            while (numbers.Count > 0)
+            {
+                Console.WriteLine("Popping '{0}'", numbers.Pop());
+            }
+
+            //Create a copy using ToArray method
+            int[] arrayNumbers = numbers.ToArray();
+
+            numbers.Push(1);
+            numbers.Push(2);
+            numbers.Push(3);
+            numbers.Push(4);
+            numbers.Push(5);
+
+            arrayNumbers = numbers.ToArray();
+
+            Console.WriteLine("Pushing elements to the array:");
+
+            foreach ( int arrayNumber in arrayNumbers)
+            {
+                Console.WriteLine(arrayNumber);
+            }
+		}
+    }
+}
